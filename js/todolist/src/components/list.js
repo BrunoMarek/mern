47ea893b93e420899a191132d01ab6b774f3b9d6
BrunoMarek <85249601+BrunoMarek@users.noreply.github.com>
2021-09-20 12:46:41 -0300

    Add files via upload

---
+++ js/todolist/src/components/list.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+
+
+const List = () => {
+    const [formInfo, setFormInfo] = useState({
+        tasks:"",
+        checked:false
+    })
+
+    const changeHandler = (e) =>{
+        setFormInfo({
+            ...formInfo,[e.target.name]:e.target.value
+        })
+    }
+
+    const [list, setList] = useState([])
+
+    const addTaskk = (e) =>{
+        e.preventDefault();
+        setList([...list,formInfo]);
+        setFormInfo({
+            tasks:"",
+            checked:false
+        })
+    }
+
+    const taskDone = (i)=>{
+        let newList = list.filter((idx)=>{
+        })
+        setList(newList);
+    }
+
+    return (
+        <>
+        <div>
+            <form onSubmit={(e)=>addTaskk(e)}>
+                <div className="form-group">
+                    <input name="tasks" onChange={(e) => changeHandler(e)} type="text" value={formInfo.tasks} />
+                </div>
+                <input className="btn btn-success" type="submit" value="Add" />
+            </form>
+            <hr />
+            
+            {list.map((taskk,i)=>{
+                const Classes =[];
+                return <div key={i} className="form-group">
+                        <p className={Classes.join(" ")} >{taskk.tasks}</p>
+                        <input type="checkbox" name="done" id="" />
+                        <button onClick={(e)=>taskDone(i)} className="btn btn-danger">Delete</button>
+                    </div>
+            })
+            }
+        </div>
+        </>
+    );
+}
+
+
+
+export default List;
